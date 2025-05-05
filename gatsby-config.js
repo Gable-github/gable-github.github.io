a26cfea4070ff383c587cea82acd6996c3e4a0ef
gable-github <gabryelsoh@gmail.com>
2025-05-04 21:05:40 -0700

    update siteurl for github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Gabryel Soh',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Gabryel Soh is a CS junior who specializes in designing, developing and deploying exceptional (most recently AI/ML) features that serve hundreds of users.',
+    siteUrl: 'https://gable-github.github.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@lostvegans',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
