463cb08c06565fa61bc97da147e9fe8f52c78f99
gable-github <gabryelsoh@gmail.com>
2025-05-04 19:56:28 -0700

    Update Hero section

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,16 +60,19 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Gabryel Soh.</h2>;
+  const three = <h3 className="big-heading">I build scalable features.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a CS junior and a passionate software engineer. I specialize in designing, developing
+        and deploying exceptional (most recently AI/ML) features that serve hundreds of users!
+        <br></br>
+        <br></br>
+        Currently, I’m building an AI-powered debugging assistant designed to integrate seamlessly
+        into your company’s CI/CD pipeline at{' '}
+        <a href="https://metabob.com/" target="_blank" rel="noreferrer">
+          metabob
         </a>
         .
       </p>
@@ -81,7 +84,7 @@ const Hero = () => {
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Recruiters, check out my resume!
     </a>
   );
 
