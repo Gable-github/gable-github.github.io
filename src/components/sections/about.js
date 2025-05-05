444a3992869c874e1566945d85aa1ae5d2f9e11d
gable-github <gabryelsoh@gmail.com>
2025-05-04 20:49:26 -0700

    update about section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,18 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'Spring boot',
+    'Java',
+    'Python',
+    'FastAPI',
+    'HuggingFace',
+    'PostgreSQL',
+    'Docker',
+    'AWS',
+    'Azure',
+    'Ansible',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,33 +146,25 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! I'm Gabryel — a backend developer passionate about building scalable AI and ML
+              features that create meaningful impact for users around the world.{' '}
+              <span role="img" aria-label="globe">
+                🌍
+              </span>
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://www.nasdaq.com/market-activity/stocks/orkt/">
+                a Nasdaq tech company
+              </a>
+              , <a href="https://plannedparenthood.org/">a major US national healthcare provider</a>
+              , and <a href="https://webai.com/">an AI orchestration startup (Series A)</a>,{' '}
+              <a href="https://metabob.com/">an enterprise AI startup</a> and{' '}
+              <a href="https://korya.co/">my own company</a>.
             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few of my favourite technologies that I’ve been working with recently:</p>
           </div>
 
           <ul className="skills-list">
