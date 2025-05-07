5393f0caaca7d29e876848c8345e9837a3fde6a2
gable-github <gabryelsoh@gmail.com>
2025-05-06 20:06:05 -0700

    Add image

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -176,7 +176,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/DSC06446_resized.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
