d9e1f54107090973e7e542cf39035bd4f157efba
Gable-github <gabryelsoh@gmail.com>
2026-09-15 11:42:47 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e1244ab65fd03677d8b7.js"
+    "url": "webpack-runtime-72b32a27396fc9b96983.js"
   },
   {
     "url": "framework-5d6349cdf9c4e4fe213c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2885dcd6b76994b77d61b26d8314f79f"
+    "revision": "c8abd6edd980b1a050588cecb082d139"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6f061b09cc63f0400406.js"
